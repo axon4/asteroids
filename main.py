1f9ac538849dcb5420af922e873c494fd602dab3
axon4 <65050809+axon4@users.noreply.github.com>
2026-05-01 17:02:37 +0100

    kill asteroids if shot

--- main.py
+++ main.py
@@ -50,6 +50,12 @@ def main():
                 print('Game over!')
                 sys.exit()
 
+            for shot in shots:
+                if shot.collides_with(asteroid):
+                    log_event('asteroid_shot')
+                    shot.kill()
+                    asteroid.kill()
+
         for sprite in drawable:
             sprite.draw(screen)
 
