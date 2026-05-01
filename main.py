488bedb10615ce17ace29309fb3c57b4b600b17d
axon4 <65050809+axon4@users.noreply.github.com>
2026-05-01 16:17:58 +0100

    create asteroids & asteroid-field, format

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+
 
 def main():
     print(f'Starting Asteroids with pygame version: {pygame.version.ver}')
@@ -16,8 +19,14 @@ def main():
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+
     Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    asteroids = pygame.sprite.Group()
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
 
     while True:
         log_state()
